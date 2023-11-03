8ee5294635840ede3f110a2a99491e8c1ed19e96
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-03 16:03:42 +0900

    commetn

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -108,6 +108,11 @@ function Login() {
           </Form>
         </Formik>
         <div className="">id: admin, pw: 123456</div>
+        <div className="text-red-400">
+          서버기한이 만료되어 연장 중이니 현재 페이지가
+          <br /> 옳바르게 작동되지 않는 점 양해 부탁드립니다.
+        </div>
+        <div className="text-red-400">긴급 복구중입니다.</div>
       </Paper>
     </div>
   )
