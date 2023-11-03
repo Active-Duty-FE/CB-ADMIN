957ee0f073e738efc64907c6762a63bf6d2803fd
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-03 17:22:59 +0900

    change server

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,5 @@
 import { AppRequest } from './axios'
 
 export const appRequest = new AppRequest({
-  baseURL: 'http://127.0.0.1:8888/api/private/v1'
+  baseURL: 'http://158.247.210.135:8888/api/private/v1'
 })
